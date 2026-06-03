bd01c8aafc6a2a4b22c36f53eca57220d2527214
El Marshall <marshallhelenm@gmail.com>
2026-06-03 13:07:28 -0700

    config fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,38 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    __VITE_WS_URL__: JSON.stringify(process.env.VITE_WS_URL || ""),
-  },
-  resolve: {
-    alias: {
-      crypto: "crypto-browserify",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const wsUrl = process.env.VITE_WS_URL || env.VITE_WS_URL || "";
+
+  return {
+    define: {
+      __VITE_WS_URL__: JSON.stringify(wsUrl),
     },
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  plugins: [react(), eslint()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
+    resolve: {
+      alias: {
+        crypto: "crypto-browserify",
+      },
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    plugins: [react(), eslint()],
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          },
         },
       },
     },
-  },
+  };
 });
